76c002777e533b11bc69829190f77ba03b61aa5d
AnyBananaGAME <ainaraspirmas@gmail.com>
2026-09-13 16:36:14 +0300

    Base build files

---
+++ build.zig
@@ -0,0 +1,24 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const server = b.dependency("dedicated_server", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const server_exe = server.artifact("DedicatedServer");
+
+    b.installArtifact(server_exe);
+
+    const run = b.addRunArtifact(server_exe);
+
+    if (b.args) |args| {
+        run.addArgs(args);
+    }
+
+    const run_step = b.step("run", "Run DedicatedServer");
+    run_step.dependOn(&run.step);
+}
